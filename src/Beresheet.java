5e3a86ce41d65177dfac29431a521fc05bb85354
adiy55 <adiyafe55@gmail.com>
2023-03-29 14:45:44 +0300

    Refactor Beresheet + add PID for hs

--- src/Beresheet.java
+++ src/Beresheet.java
@@ -2,14 +2,69 @@ import simulation.Moon;
 
 public class Beresheet {
     public static final double WEIGHT_EMP = 165; // kg
-    public static final double WEIGHT_FULE = 420; // kg
-    public static final double WEIGHT_FULL = WEIGHT_EMP + WEIGHT_FULE; // kg
+    public static final double WEIGHT_FUEL = 420; // kg
+    public static final double WEIGHT_FULL = WEIGHT_EMP + WEIGHT_FUEL; // kg
     // https://davidson.weizmann.ac.il/online/askexpert/%D7%90%D7%99%D7%9A-%D7%9E%D7%98%D7%99%D7%A1%D7%99%D7%9D-%D7%97%D7%9C%D7%9C%D7%99%D7%AA-%D7%9C%D7%99%D7%A8%D7%97
-    public static final double MAIN_ENG_F = 430; // N
-    public static final double SECOND_ENG_F = 25; // N
-    public static final double MAIN_BURN = 0.15; //liter per sec, 12 liter per m'
-    public static final double SECOND_BURN = 0.009; //liter per sec 0.6 liter per m'
-    public static final double ALL_BURN = MAIN_BURN + 8 * SECOND_BURN;
+    public static final double MAIN_ENG_F = 430; // N // Main engine
+    public static final double SECOND_ENG_F = 25; // N // Side engine
+    public static final double MAIN_BURN = 0.15; // liter per sec, 12 liter per m'
+    public static final double SECOND_BURN = 0.009; // liter per sec 0.6 liter per m'
+    public static final double ALL_BURN = MAIN_BURN + 8 * SECOND_BURN; // Total fuel consumption // 8 side engines
+    public static final double TOTAL_THRUST = MAIN_ENG_F + 8 * SECOND_ENG_F;
+
+    private double _hs, _vs, _alt, _NN, _ang, _dist, _time, _acc, _fuel, _weight;
+    private boolean _first;
+
+    Beresheet() {
+        _hs = 932;
+        _vs = 24.8;
+        _alt = 13748; // 2:25:40 (as in the simulation) // https://www.youtube.com/watch?v=JJ0VfRL9AMs
+        _NN = 0.7; // rate[0,1]
+        _ang = 58.3; // zero is vertical (as in landing)
+        _dist = 181 * 1000;
+        _time = 0;
+        _acc = 0; // Acceleration rate (m/s^2)
+        _fuel = 121; //
+        _weight = WEIGHT_EMP + _fuel;
+        _first = true; // for toString method
+    }
+
+    @Override
+    public String toString() {
+        if (_first) {
+            System.out.println("Simulating Bereshit's Landing:");
+            System.out.println("time, vs, hs, dist, alt, ang, weight, acc");
+            _first = false;
+        }
+        return String.format("%f, %f, %f, %f, %f, %f, %f, %f", _time, _vs, _hs, _dist, _alt, _ang, _weight, _acc);
+    }
+
+    public double getHs() {
+        return _hs;
+    }
+
+    public double getVs() {
+        return _vs;
+    }
+
+    public double getAlt() {
+        return _alt;
+    }
+
+    public double getAng() {
+        return _ang;
+    }
+
+    public void addAng(double val) {
+        double tmp = _ang + val;
+        if (tmp < 90 && tmp > 0) {
+            _ang = tmp;
+        }
+    }
+
+    public double getTime() {
+        return _time;
+    }
 
     public static double accMax(double weight) {
         return acc(weight, true, 8);
@@ -24,109 +79,108 @@ public class Beresheet {
         return t / weight;
     }
 
-    public static double desired_vs(double alt) {
+    public double getDesiredVs() {
         double max_alt = 30000;
-        if (alt > max_alt) {
+        if (_alt > max_alt) {
             return 0;
         }
-        if (alt > 1000) {
+        if (_alt > 1000) {
             return 23;
         }
-        if (alt > 500) {
-            return 13 + 10 * (alt - 500) / 500;
+        if (_alt > 500) {
+            return 13 + 10 * (_alt - 500) / 500;
         }
-        if (alt > 70) {
-            return 5 + 8 * (alt - 70) / 430;
+        if (_alt > 70) {
+            return 5 + 8 * (_alt - 70) / 430;
         }
         return 2;
     }
 
-    public static double desired_hs(double alt) {
+    public double getDesiredHs() {
         double min_alt = 2000, max_alt = 30000;
-        if (alt < min_alt) {
+        if (_alt < min_alt) {
             return 0;
         }
-        if (alt > max_alt) {
+        if (_alt > max_alt) {
             return Moon.EQ_SPEED;
         }
-        double norm = (alt - min_alt) / (max_alt - min_alt);
+        double norm = (_alt - min_alt) / (max_alt - min_alt);
         norm = Math.pow(norm, 0.70); // [0,1]
         return norm * Moon.EQ_SPEED;
     }
 
-    public static double updatePower(double curr_power, double gas) {
-        double power = curr_power + gas;
-        if (power < 0) {
-            return 0;
-        } else if (power > 1) {
-            return 1;
+    public void addPower(double gas) {
+        double tmp = _NN + gas;
+        if (tmp >= 0 && tmp <= 1) {
+            _NN = tmp;
         }
-        return power;
+    }
+
+    public void computeStep(double dt) {
+        // main computations
+        double ang_rad = Math.toRadians(_ang);
+        double h_acc = Math.sin(ang_rad) * _acc;
+        double v_acc = Math.cos(ang_rad) * _acc;
+        double vacc = Moon.getAcc(_hs);
+        _time += dt;
+        double dw = dt * ALL_BURN * _NN;
+        if (_fuel > 0) {
+            _fuel -= dw;
+            _weight = WEIGHT_EMP + _fuel;
+            _acc = _NN * accMax(_weight);
+        } else { // ran out of fuel
+            _acc = 0;
+        }
+        v_acc -= vacc;
+        if (_hs > 0) {
+            _hs -= h_acc * dt;
+        } else if (_hs < 2.5) {
+            _hs = 0;
+        }
+        _dist -= _hs * dt;
+        _vs -= v_acc * dt;
+        _alt -= dt * _vs;
     }
 
     // 14095, 955.5, 24.8, 2.0
     public static void main(String[] args) {
-        System.out.println("Simulating Bereshit's Landing:");
+        Beresheet beresheet = new Beresheet();
         // starting point:
-        double vs = 24.8;
-        double hs = 932;
-        double dist = 181 * 1000;
-        double ang = 58.3; // zero is vertical (as in landing)
-        double alt = 13748; // 2:25:40 (as in the simulation) // https://www.youtube.com/watch?v=JJ0VfRL9AMs
-        double time = 0;
         double dt = 1; // sec
-        double acc = 0; // Acceleration rate (m/s^2)
-        double fuel = 121; //
-        double weight = WEIGHT_EMP + fuel;
+        double vs, hs, ang;
+        double dvs, dhs, gas;
 
         PID vsPID = new PID(0.04, 0.0003, 0.2, 100);
-        double dvs, gas;
+        PID hsPID = new PID(0.0006, 0.0, 0.0, 950);
 
-        System.out.println("time, vs, hs, dist, alt, ang, weight, acc");
-        double power = 0.7; // rate[0,1]
         // ***** main simulation loop ******
+        while (beresheet.getAlt() > 0) {
+            beresheet.computeStep(dt);
+            vs = beresheet.getVs();
+            hs = beresheet.getHs();
 
-        while (alt > 0) {
-            if (time % 10 == 0 || alt < 100) {
-                System.out.println(time + "," + vs + "," + hs + "," + dist + "," + alt + "," + ang + "," + weight + "," + acc);
-            }
-
-            dvs = desired_vs(alt);
+            // 2 km above the surface
+            dvs = beresheet.getDesiredVs();
             gas = vsPID.update(vs - dvs, dt);
-            power = updatePower(power, gas);
+            beresheet.addPower(gas);
 
-            if (Math.abs(hs) < 3) {
-                if (ang > 3) {
-                    ang -= 3;
-                } else {
-                    ang = 0;
-                }
+            dhs = beresheet.getDesiredHs();
+            ang = hsPID.update(hs - dhs, dt);
+            beresheet.addAng(ang);
+
+            if (beresheet.getTime() % 10 == 0 || beresheet.getAlt() < 100) {
+                System.out.println(beresheet);
             }
 
-            // main computations
-            double ang_rad = Math.toRadians(ang);
-            double h_acc = Math.sin(ang_rad) * acc;
-            double v_acc = Math.cos(ang_rad) * acc;
-            double vacc = Moon.getAcc(hs);
-            time += dt;
-            double dw = dt * ALL_BURN * power;
-            if (fuel > 0) {
-                fuel -= dw;
-                weight = WEIGHT_EMP + fuel;
-                acc = power * accMax(weight);
-            } else { // ran out of fuel
-                acc = 0;
-            }
-
-            v_acc -= vacc;
-            if (hs > 2.5) {
-                hs -= h_acc * dt;
-            } else {
-                hs = 0;
-            }
-            dist -= hs * dt;
-            vs -= v_acc * dt;
-            alt -= dt * vs;
+//            // if hs is less than 3, angle should be zero (for landing)
+//            if (Math.abs(hs) < 3) {
+//                ang = beresheet.getAng();
+//                if (ang >= 3 * dt) {
+//                    beresheet.addAng(-3 * dt);
+//                } else {
+//                    beresheet.addAng(-ang);
+//                }
+//            }
         }
     }
 }
